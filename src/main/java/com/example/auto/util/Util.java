973f386526d44b1c697d533f687433095fe9c9f2
Kulwinder Singh <68424283+kulwinder-singh17@users.noreply.github.com>
2024-09-07 17:30:31 +0530

    Update Util.java

--- src/main/java/com/example/auto/util/Util.java
+++ src/main/java/com/example/auto/util/Util.java
@@ -1,21 +1,13 @@
 package com.example.auto.util;
 
-
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Util {
 
     public static String getEnv(String key) {
         try {
-            String value = "";
-            if (System.getProperty(key) != null && System.getProperty("browser").length() > 0) {
-                value = System.getProperty(key);
-            } else {
-                Dotenv dotenv = Dotenv.configure().directory(System.getProperty("user.dir") + "/").filename("secret.env")
-                        .load();
-                value = dotenv.get(key);
-            }
-            return value;
+            Dotenv dotenv = Dotenv.configure().directory(System.getProperty("user.dir") + "/").filename("secret.env").load();
+            return dotenv.get(key);
         } catch (Exception e) {
             System.err.println("Not able to fetch env value.");
             return null;
